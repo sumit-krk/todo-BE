aed330ca9b03150eb6d981704d855304ce890430
sumit kumar <sumit.kumar@marktine.com>
2026-01-03 01:29:06 +0530

    edit profile api done

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -25,5 +25,34 @@ profileRoutes.get("/get-profile",verifyUser,async(req, res)=>{
    }
 })
 
+profileRoutes.patch("/edit-profile",verifyUser, async(req, res)=>{
+    try{
+        const userData=req.user;
+        if(!userData){
+            throw new Error("User not found");
+        }
+        const allowedEditFields = ["firstName", "lastName", "email", "photoUrl"];
+        const isEditAllowed = Object.keys(req.body).every(field => allowedEditFields.includes(field));
+        if(!isEditAllowed){
+            throw new Error("Editing some fields is not allowed");
+        }
+        Object.keys(req.body).forEach(field => {
+            userData[field] = req.body[field];
+        });
+        const updatedUser=await userData.save();
+        res.status(200).json({
+            success:true,
+            message:"User profile edited successfully",
+            data:updatedUser
+        })
+    }catch(err){
+        res.status(500).json({
+            success:false,
+            message:"Error in editing user profile",
+            error:err.message
+        })
+    }
+})
+
 module.exports={profileRoutes}
 
