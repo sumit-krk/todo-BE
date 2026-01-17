60d139d0e51433b76871b8398eb03bfdb55bed71
sumit kumar <sumit.kumar@marktine.com>
2026-01-17 22:08:00 +0530

    small fixes

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app=express();
 // console.log("Port:", PORT);
 
 app.use(cors({
-    origin:"https://localhost:5173",
+    origin:"http://localhost:5173",
     credentials:true,
 }));
 app.use(express.json());
